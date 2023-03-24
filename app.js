152054cd1701d90d0e34760480e6f246812f1a2a
Ryan Luker <ryanluker@outlook.com>
2023-03-24 16:34:35 +0000

    Adjust code to work with Greenspace

--- app.js
+++ app.js
@@ -75,7 +75,7 @@
     );
 
     var host =
-      signInEl.dataset.host || 'https://app.wellnesstogether.grnspace.ca/';
+      signInEl.dataset.host || 'https://app.staging.grnspace.ca/';
     if (host[host.length - 1] !== '/') host += '/';
     fetch(host + 'oauth2/token', {
       method: 'POST',
